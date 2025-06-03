793a28c027c126ae0d59d68b9c2f26af0d25a215
RaidanRR <edward5raidan5497@gmail.com>
2025-06-03 11:42:39 +0700

    some change

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,27 @@
 <?php
 // routes/web.php
-
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return redirect('/dashboard');
-});
+// Redirect root ke dashboard
+Route::get('/', fn () => redirect('/dashboard'));
 
-// Hanya satu route dengan name 'dashboard'
+// Dashboard (1 name only)
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+// Profile (hanya untuk user yang sudah login)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/login/keycloak', function () {
-    return Socialite::driver('keycloak')->redirect();
-});
+// Keycloak SSO login & callback
+Route::get('/login/keycloak', fn () => Socialite::driver('keycloak')->redirect())->name('login.keycloak');
 
 Route::get('/login/keycloak/callback', function () {
     $keycloakUser = Socialite::driver('keycloak')->stateless()->user();
